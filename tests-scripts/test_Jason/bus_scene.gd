5051dc5f92cc2c0a82da33b1bb98b1874db06967
JasonJ13 <jason.jallabert@orange.fr>
2025-10-20 01:31:23 +0200

    ça mérite pas un merge je pense

--- tests-scripts/test_Jason/bus_scene.gd
+++ tests-scripts/test_Jason/bus_scene.gd
@@ -2,6 +2,7 @@ extends ColorRect
 
 @onready var animation2 : AnimationPlayer = $Bus2/AnimationBus
 @onready var animation3 : AnimationPlayer = $Bus3/AnimationBus
+@onready var timerstart : Timer = $Timer
 
 var n_case = 1
 
@@ -13,21 +14,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
-	
-	if Input.is_action_just_pressed("ui_accept") && n_case == 1:
+	if Input.is_action_just_pressed("ui_accept") && n_case == 2 :
+		animation3.play("case_in")
+		n_case = 3
+
+	if Input.is_action_just_pressed("ui_accept") && n_case == 1 && timerstart.is_stopped():
 		animation2.play("case_in")
 		n_case = 2
-		
-		
-	if Input.is_action_just_pressed("ui_accept") && n_case == 3 :
-		animation3.play("case_in")
-		n_case = 4
 
 
 func In2_finished(anim_name: StringName) -> void:
+	
 	if anim_name == 'case_in' :
 		animation2.play("busAnimation")
-		n_case = 3
 
 
 func In3_finished(anim_name: StringName) -> void:
