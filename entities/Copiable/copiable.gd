379cafa367a0ccabc3c14b7d03f4b69c71eaebbc
Helo1204 <heloise.havy@gmail.com>
2025-10-19 20:41:45 +0200

    Simplification du système de transformation en objet

---
+++ entities/Copiable/copiable.gd
@@ -0,0 +1,3 @@
+class_name Copiable extends StaticBody3D
+
+@export var meshInstance : MeshInstance3D
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -71,7 +71,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("change_view"):
 		object_view=!object_view
 		if object_view:
-			if copy_mesh():
+			if change_to_object():
 				camera.position=$ObjectViewPoint.position
 			else:
 				object_view!=object_view
@@ -90,73 +90,32 @@ func _process(delta: float) -> void:
 			
 		
 		
-func copy_mesh()->bool:
+func change_to_object()->bool:
 	raycast.global_transform.origin = camera.global_transform.origin
 	raycast.global_transform.basis = camera.global_transform.basis
 
  
 	raycast.force_raycast_update()  # s'assure que le raycast est à jour
 	if raycast.is_colliding():
-		var collider = raycast.get_collider()
-		
-			# Vérifier si l'objet a un MeshInstance
-		var mesh_instance = find_mesh_instance(collider)
-		if not mesh_instance.is_in_group("copiable"):
-			print("Objet non copiable")
-			return false
-		if mesh_instance:
-			# Copier le mesh
-			var player_mesh = $MeshInstance3D  # ton mesh actuel du joueur
-			player_mesh.mesh = mesh_instance.mesh
-			print("Joueur transformé en l'objet touché !")
-
-			# Si tu veux, tu peux aussi ajuster le collider du joueur
-			var shape = mesh_instance.mesh.create_convex_shape()
-			if shape:
-				var collision_shape = $CollisionShape3D  # collider actuel du joueur
-				var old_center = collider_offset_y  # centre précédent
-				collision_shape.shape = shape
-				var new_center = get_collider_center_y(shape,mesh_instance.mesh)
-
-				# Ajuster la position verticale pour compenser
-				position.y += (new_center - old_center)
-				collider_offset_y = new_center  # mettre à jour l'offset
-	
-				print("Collider du joueur mis à jour !")
-				return(true)
+		var collide = raycast.get_collider()
+		if collide is Copiable:
+			$MeshInstance3D.mesh  = collide.meshInstance.mesh
+			var shape = $MeshInstance3D.mesh.create_convex_shape()
+			$CollisionShape3D.shape=shape
+			
+			#Déplacer le center pour pas tomber à travers le sol
+			var old_center = collider_offset_y  
+			var new_center = get_collider_center_y(shape,collide.meshInstance.mesh)
+			position.y += (new_center - old_center)
+			collider_offset_y = new_center  
+			return(true)
+				
 				
 		else:
 			print("Pas de MeshInstance trouvé sur cet objet.")
 	return false
-
-
-func find_mesh_instance_in_children(node: Node) -> MeshInstance3D:
-	for child in node.get_children():
-		var found = find_mesh_instance_in_children(child)
-		if found:
-			return found
-	return null
 	
-func find_mesh_instance(collider: Node) -> MeshInstance3D:
-	var node = collider
-	while node:
-		if node is MeshInstance3D:
-			return node
-		node = node.get_parent()
-	return find_mesh_instance_in_children(collider)
-	
-	
-func draw_ray(start_pos: Vector3, end_pos: Vector3, color: Color):
-	var line = ImmediateMesh.new()
-	line.surface_begin(Mesh.PRIMITIVE_LINES)
-	line.surface_set_color(color)
-	line.surface_add_vertex(start_pos)
-	line.surface_add_vertex(end_pos)
-	line.surface_end()
 
-	var mesh_instance = MeshInstance3D.new()
-	mesh_instance.mesh = line
-	add_child(mesh_instance)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
